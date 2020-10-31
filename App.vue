e44e7b90e302adbe4c10bf42302836aeb309fa01
JuliusDeng <1576554007@qq.com>
2020-10-31 15:56:55 +0800

    uni-app、weex、egg.js开发在线直播APP（兼容微信小程序端）

---
+++ App.vue
@@ -0,0 +1,33 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			//  weex图标使用
+			const domModule = weex.requireModule('dom')
+			domModule.addRule('fontFace', {
+			    'fontFamily': "iconfont",
+			    'src': "url('http://at.alicdn.com/t/font_1859985_7mxozsfdvib.ttf')"
+			});
+			// 监听底部导航中间凸起按钮
+			uni.onTabBarMidButtonTap( () => {
+				console.log('tab中间点击');
+			})
+			
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import url("/common/free.css");
+	@import url("/common/common.css");
+	/* #ifndef APP-NVUE */
+	@import url("/common/icon.css");
+	/* #endif */
+</style>
